f46a67d074a4d6ee16f4744358b5d7fb2fcab2f0
SOLARIS-bit <shaltonkennedy@gmail.com>
2025-10-23 10:14:51 +0200

    Improve subtract function with debug print

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ def add(*args):
     return sum(args)
 
 def subtract(*args):
+    """Subtract an unlimited number of numbers."""
+    print("Executing improved subtract function...")
     if not args:
         return 0
     result = args[0]
