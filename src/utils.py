536fd1dea5cf8a8d5af06389f9a6fa61cd38b500
SOLARIS-bit <shaltonkennedy@gmail.com>
2025-10-23 23:46:57 +0200

    Work in progress: utils improvements

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,6 @@ def add(*args):
     return sum(args)
 
 def subtract(*args):
-    """Subtract an unlimited number of numbers."""
-    print("Executing improved subtract function...")
     if not args:
         return 0
     result = args[0]
@@ -11,3 +9,18 @@ def subtract(*args):
         result -= num
     return result
 
+def multiply(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+def divide(*args):
+    if not args:
+        return 0
+    result = args[0]
+    for num in args[1:]:
+        if num == 0:
+            return "Error: Division by zero"
+        result /= num
+    return result
