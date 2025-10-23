293663a16a6c1e3dd03bde1f8d94495e7718fe11
SOLARIS-bit <shaltonkennedy@gmail.com>
2025-10-23 10:12:41 +0200

    Improve add function with debug print

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,29 @@
 def add(*args):
+    """Add an unlimited number of numbers."""
+    print("Executing improved add function...")
     return sum(args)
 
+
 def subtract(*args):
+    """Subtract an unlimited number of numbers."""
     if not args:
         return 0
     result = args[0]
     for num in args[1:]:
         result -= num
     return result
-    
+
+
 def multiply(*args):
+    """Multiply an unlimited number of numbers."""
     result = 1
     for num in args:
         result *= num
     return result
 
+
 def divide(*args):
+    """Divide sequentially all provided numbers."""
     try:
         result = args[0]
         for num in args[1:]:
@@ -23,4 +31,3 @@ def divide(*args):
         return result
     except ZeroDivisionError:
         return "Error: Division by zero"
-
