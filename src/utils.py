0765684d83003f2fd411d56cce6daa59dae552a9
SOLARIS-bit <shaltonkennedy@gmail.com>
2025-10-23 09:50:50 +0200

    Add multiply and divide functions

--- src/utils.py
+++ src/utils.py
@@ -8,4 +8,19 @@ def subtract(*args):
     for num in args[1:]:
         result -= num
     return result
+    
+def multiply(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+def divide(*args):
+    try:
+        result = args[0]
+        for num in args[1:]:
+            result /= num
+        return result
+    except ZeroDivisionError:
+        return "Error: Division by zero"
 
